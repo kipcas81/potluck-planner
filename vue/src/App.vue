25eef84c04b2e30c5c7ff6cefbad09d7022cac3e
Bill Greenrod <billifordgrod@gmail.com>
2024-04-12 15:07:10 -0400

    fuck you

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,7 +4,7 @@
       <!-- need pipe -->
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>&nbsp;|&nbsp;
-      <!-- <router-link v-bind:to="{name: 'friends'}">Friends</router-link> -->
+      <router-link v-bind:to="{name: 'friends'}">Friends</router-link>
     </div>
     <router-view />
   </div>
