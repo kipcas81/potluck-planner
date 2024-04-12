ae08ea54b217ed9d2d17e25ce8fedd7ed7366ea4
Bill Greenrod <billifordgrod@gmail.com>
2024-04-12 13:14:20 -0400

    how

--- vue/src/views/PotluckView.vue
+++ vue/src/views/PotluckView.vue
@@ -1,53 +1,49 @@
 <template>
-    <div>
-      <!-- this is where the potluck-detail will go -->
-    </div>
+  <div>
+    <!-- this is where the potluck-detail will go -->
+  </div>
 </template>
-
-
 <script>
 // import potluck detail
 import PotluckService from '../services/PotluckService';
-
+import PotluckDetail from '../components/PotluckDetail.vue';
 export default {
-    components: {
-        // potluck detail
-    },
-    data(){
-      return {
-        Potluck: {
-                name: "",
-                description: "", 
-                date: "",
-                time: "",
-                diet: [
-                { id: 'vegan', label: 'Vegan', checked: false },
-                { id: 'vegetarian', label: 'Vegetarian', checked: false},
-                { id: 'gluten', label: 'Gluten-Free', checked: false },
-                { id: 'sodium', label: 'Low-Sodium', checked: false },
-                { id: 'paleo', label: 'Paleo', checked: false },
-                { id: 'halal', label: 'Halal', checked: false },
-                { id: 'kosher', label: 'Kosher', checked: false },
-                { id: 'dairy', label: 'Dairy-Free', checked: false },
-                { id: 'nuts', label: 'No-Nuts', checked: false },
-                { id: 'sugar', label: 'Sugar-Free', checked: false}
-                ],
-                isRecurring: false,
-                frequency: 0,
-                location: "",
-                isPrivate: false
-            },
-      };
-    },
-    created(){
-      PotluckService.getPotluck(this.$route.params.potluckId)
-      .then(response => {
-        this.potluck = response.data;
-      });
-    }
+  components: {
+      // potluck detail
+  },
+  data(){
+    return {
+      Potluck: {
+              name: "",
+              description: "",
+              date: "",
+              time: "",
+              diet: [
+              { id: 'vegan', label: 'Vegan', checked: false },
+              { id: 'vegetarian', label: 'Vegetarian', checked: false},
+              { id: 'gluten', label: 'Gluten-Free', checked: false },
+              { id: 'sodium', label: 'Low-Sodium', checked: false },
+              { id: 'paleo', label: 'Paleo', checked: false },
+              { id: 'halal', label: 'Halal', checked: false },
+              { id: 'kosher', label: 'Kosher', checked: false },
+              { id: 'dairy', label: 'Dairy-Free', checked: false },
+              { id: 'nuts', label: 'No-Nuts', checked: false },
+              { id: 'sugar', label: 'Sugar-Free', checked: false}
+              ],
+              isRecurring: false,
+              frequency: 0,
+              location: "",
+              isPrivate: false
+          },
+    };
+  },
+  created(){
+    PotluckService.getPotluck(this.$route.params.potluckId)
+    .then(response => {
+      this.potluck = response.data;
+    });
+  }
 }
 </script>
-
-
 <style>
 </style>
