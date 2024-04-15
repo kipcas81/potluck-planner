508f1c4b6ffe4333378dbac8c88e762c27ceeacd
kip castillo <kipcastillo@gmail.com>
2024-04-15 10:28:06 -0400

    update dietary restriction in DB, viewe past and future potlucks, and deleate potlucks

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,6 +3,6 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role,email,first_name,last_name) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', 'jfish@gmail.com', 'john', 'fish');
 INSERT INTO users (username,password_hash,role,email,first_name,last_name) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'holyfuck@gmail.com', 'dog', 'doggie');
 
-INSERT INTO potlucks(event_name, description, event_date,event_time,user_id,potluck_dietary_restrictions,is_recurring,frequency_days,location,is_private,is_completed)
-VALUES ('Two Prawn Approach', 'this is a new potluck','05/05/2022', '11:55 AM', '1', '{"vegan"}', false, 0, 'here', true,false);
+INSERT INTO potlucks(event_name, description, event_date, event_time, user_id, potluck_dietary_restrictions, is_recurring, frequency_days, location, is_private, is_completed)
+VALUES ('Two Prawn Approach', 'this is a new potluck','05/25/2024', '0700', '1', 'vegan', false, 0, 'here', true,false);
 COMMIT TRANSACTION;
