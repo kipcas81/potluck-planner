b59498cee2224744c8fc62c187464e31ca4eece3
Bill Greenrod <billifordgrod@gmail.com>
2024-04-16 15:15:32 -0400

    potluck view cosmetics

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -13,18 +13,21 @@
 <style >
 #capstone-app {
   background-image: url("./assets/2-prawn-pattern.png"); 
-  background-size: contain;  
-  height: 100%;
+  background-size: cover;  
+  background-attachment: fixed; /* Background fixed so it won't scroll with content */
   background-position: center; 
-  background-repeat: repeat; 
+  background-repeat: no-repeat; 
   font-family: cursive;
-  
+  min-height: 100vh; /* Set minimum height to fill the viewport */
+  position: relative; /* Add position relative to enable z-index for other content */
 }
 
 #nav {
   background-color: rgba(255, 255, 255, 0.7); 
   padding: 10px;
- 
+  position:sticky;
+  top: 0;
+  z-index: 1;
 }
 
 </style>
