1c21470c8ef78bef9021ea5a3be36a1b6a1dd3cb
kip castillo <kipcastillo@gmail.com>
2024-04-18 08:23:32 -0400

    did stuff

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -74,7 +74,7 @@ CREATE TABLE potluck_guests (
 	guest_id SERIAL,
 	guest_first_name varchar (50),
 	guest_last_name varchar (50),
-	guest_email_address varchar(50) NOT NULL UNIQUE,
+	guest_email_address varchar(50) NOT NULL,
 	guest_already_registered boolean default false,
 	CONSTRAINT PK_potluck_guests PRIMARY KEY (potluck_id, guest_id),
 	CONSTRAINT FK_potlucks_potluck_guests FOREIGN KEY (potluck_id) REFERENCES potlucks(potluck_id));
